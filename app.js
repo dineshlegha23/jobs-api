149f09fc3d949207cc07a7c417543abc43db8774
Dinesh <dineshlegha23@gmail.com>
2024-07-27 13:08:57 +0530

    added security packages

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const connectDB = require("./config/db");
 require("dotenv").config();
 require("express-async-errors");
+
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+
 const express = require("express");
 const app = express();
 const jobsRouter = require("./routes/jobs");
@@ -10,7 +16,16 @@ const authMiddleware = require("./middleware/authentication");
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+  })
+);
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 app.use("/api/v1/jobs", authMiddleware, jobsRouter);
 app.use("/api/v1/auth", authRouter);
